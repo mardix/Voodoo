d781f36d633079b6107e3dcab1e48c813f4cd6e1
Mardix <mcx2082@gmail.com>
2012-08-29 04:07:15 -0400

    Fix sendToEmail to send exception to an  email address

--- Voodoo/Core/Exception.php
+++ Voodoo/Core/Exception.php
@@ -12,7 +12,6 @@
  * -----------------------------------------------------------------------------
  * 
  * @name        Core\Exception
- * @since       Apr 23, 2011
  * @desc        Exception
  * 
  */
@@ -21,16 +20,76 @@ namespace Voodoo\Core;
 
 class Exception extends \Exception{
     
-    /**
-     * To notify admin of an exception 
-     */
-    public function notifyAdmin($email){
-        
-    }
-    
     public function __toString(){
         return "Message: ".$this->getMessage()." - Code:".$this->getCode();
     }
+    
+    
+    
+    /**
+     * Allow the exception to be sent to an email address
+     * 
+     * @param string $email 
+     */
+    public function sendToEmail($email){
+
+    $_post = print_r($_POST,true);
+    $_get = print_r($_GET,true);
+    $_cookie = print_r($_COOKIE,true);
+    $_request = print_r($_REQUEST,true);
+    $_server = print_r($_SERVER,true);
+    $_e = print_r($this,true);
+
+$message = "
+VoodooPHP is reporting an exception error in your application
+
+Server: {$_SERVER["HTTP_HOST"]}
+
+Date: ".date("Y-m-d H:i:s")."
+
+
+--------------------------------------------------------------------------------
+
+>> EXCEPTION MESSAGE
+".$this->__toString()."
+
+--------------------------------------------------------------------------------
+        
+>> EXCEPTION: 
+$_e
+
+--------------------------------------------------------------------------------
+        
+>> SERVER: 
+{$_server}
+
+--------------------------------------------------------------------------------
+
+>> REQUEST: 
+{$_request}
+
+--------------------------------------------------------------------------------
+
+>> POST : 
+{$_post}
+
+--------------------------------------------------------------------------------
+
+>> GET:
+{$_get}
+
+--------------------------------------------------------------------------------
+
+>> COOKIE: 
+{$_cookie}
+
+"; 
+     // Send
+     @mail($email, "{$_SERVER["HTTP_HOST"]} : EXCEPTION ERROR CAUGHT ", $message);
+
+    }
+    
+
 }
 
 
