fdf77f115e1900a5bcf2f257d41eec4f86212ffc
Mardix <mcx2082@gmail.com>
2012-11-22 03:01:47 -0500

    Addition of the annotation @request. Refactoring

--- src/Voodoo/Core/Controller/Api.php
+++ src/Voodoo/Core/Controller/Api.php
@@ -21,7 +21,8 @@
 
 namespace Voodoo\Core\Controller;
 
-use Voodoo\Core;
+use Voodoo\Core,
+    Closure;
 
 abstract class Api extends Core\Controller
 {
@@ -99,54 +100,21 @@ abstract class Api extends Core\Controller
     }
 
     /**
-     * Accept POST
+     * Check if a request method is the one provided.
+     * If failed it will set 405 error
      *
+     * @param string $method - (GET | POST | PUT | DELETE)
+     * @param string $errorMessage - The error message if failed
      * @return bool
      */
-    protected function acceptPost()
-    {
-        return $this->acceptMethod("POST");
-    }
-
-    /**
-     * Accept POST
-     *
-     * @return bool
-     */
-    protected function acceptGet()
-    {
-        return $this->acceptMethod("GET");
-    }
-
-    /**
-     * Accept POST
-     *
-     * @return bool
-     */
-    protected function acceptDelete()
-    {
-        return $this->acceptMethod("DELETE");
-    }
-
-    /**
-     * Accept POST
-     *
-     * @return bool
-     */
-    protected function acceptPut()
-    {
-        return $this->acceptMethod("PUT");
-    }
-
-    /**
-     * Accept a method
-     *
-     * @return bool
-     */
-    private function acceptMethod($method)
+    private function requestMethod($method, $errorMessage="")
     {
         if (!Core\Http\Request::is($method)) {
             $this->setHttpCode(405);
+
+            if($errorMessage) {
+                $this->view()->setError($errorMessage);
+            }
             return false;
         }
         return true;
@@ -157,9 +125,47 @@ abstract class Api extends Core\Controller
      */
     public function action_404()
     {
-        $this->setHttpCode(404);
+        $errorCode = 404;
+        $message = Core\Http\Response::getHttpCode($errorCode);
 
-        $message = Core\Http\Response::getHttpCode(404);
-        $this->view()->setError($message, 404);
+        $this->setHttpCode($errorCode);
+        $this->view()->setError($message, $errorCode);
+    }
+
+
+    /**
+     * To execute an action. 
+     * But unlike the Core\Controller::getAction()
+     * Api::getAction() checks for the '@request' annotation in the action.
+     * The @request annotation is array containing the keys: method, response
+     * - Annotation
+     *      @request Array
+     * - arguments
+     *      method (POST|GET|PUT|DELETE) - The request method to accept
+     *      response - a message to display if the request method fails
+     * - example: 
+     *      @request [method=POST, response="This is an error message"]
+     * 
+     * @param string $action
+     * @return Core\Controller
+     */
+    public function getAction($action = "index")
+    {
+        // Set the action to get the annotation
+        $this->setActionName($action);
+
+        $request = $this->getActionAnnotation("request");
+
+        if(is_array($request) && $request["method"]) {
+            $response = $request["response"] ?: "";
+            if (!$this->requestMethod($request["method"], $response)) {
+                return null;
+            } else {
+                return parent::getAction($action);
+            }
+        } else {
+            return parent::getAction($action);
+        }
+
     }
 }
