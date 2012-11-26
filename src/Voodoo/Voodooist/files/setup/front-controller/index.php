33deccbdc3d8fc098faf9fdc4b0d756da10029a3
Mardix <mcx2082@gmail.com>
2012-11-26 09:27:49 -0500

    change name in setup front controller

--- src/Voodoo/Voodooist/files/setup/front-controller/index.php
+++ src/Voodoo/Voodooist/files/setup/front-controller/index.php
@@ -21,19 +21,19 @@ $VoodooPHP_Dir = __DIR__;
 require($VoodooPHP_Dir."/Voodoo/init.php");
 
     /**
-     * Set the default app to access. By default it's Www
+     * Set the application name to use. By default it's Www
      * @type string
      */
-    $application = "www";
+    $appName = "www";
 
     /**
      * The URI
      * @type string
      */
-    $segments = implode("/",Voodoo\Core\Http\Request::getUrlSegments());
+    $uri = implode("/",Voodoo\Core\Http\Request::getUrlSegments());
 
     /**
      * Let's do it!
      */
-    (new Voodoo\Core\Voodoo($application, $segments))->doMagic();
+    (new Voodoo\Core\Voodoo($appName, $uri))->doMagic();
     
