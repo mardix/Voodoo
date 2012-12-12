75ee29ba5b5af109fafd4c8e114ae647458fe018
Mardix <mcx2082@gmail.com>
2012-12-12 02:02:23 -0500

    Refactor and moved annotationi @request to Core/Controller

--- src/Voodoo/Core/Controller/Api.php
+++ src/Voodoo/Core/Controller/Api.php
@@ -21,8 +21,7 @@
 
 namespace Voodoo\Core\Controller;
 
-use Voodoo\Core,
-    Closure;
+use Voodoo\Core;
 
 abstract class Api extends Core\Controller
 {
@@ -38,22 +37,6 @@ abstract class Api extends Core\Controller
         parent::init();
         $this->setJsonResponse();
     }
-
-
-    /**
-     * Finalize
-     *
-     * Do some assignment and finalize the process
-     */
-    protected function finalize()
-    {
-        parent::finalize();
-
-        if($this->view()->hasError()) {
-            $this->view()->assign("error", $this->view()->getMessage("error"));
-        }
-    }
-
 
     /**
      *
@@ -99,27 +82,6 @@ abstract class Api extends Core\Controller
     }
 
     /**
-     * Check if a request method is the one provided.
-     * If failed it will set 405 error
-     *
-     * @param string $method - (GET | POST | PUT | DELETE)
-     * @param string $errorMessage - The error message if failed
-     * @return bool
-     */
-    private function requestMethod($method, $errorMessage="")
-    {
-        if (!Core\Http\Request::is($method)) {
-            $this->setHttpCode(405);
-
-            if($errorMessage) {
-                $this->view()->setError($errorMessage);
-            }
-            return false;
-        }
-        return true;
-    }
-
-    /**
      * action_404() will be invoked if an action is missing
      */
     public function action_404()
@@ -129,41 +91,5 @@ abstract class Api extends Core\Controller
 
         $this->setHttpCode($errorCode);
         $this->view()->setError($message, $errorCode);
-    }
-
-
-    /**
-     * To execute an action. 
-     * But unlike the Core\Controller::getAction()
-     * Api::getAction() checks for the '@request' annotation in the action.
-     * The @request annotation is array containing the keys: method, response
-     * - Annotation
-     *      @request Array
-     * - arguments
-     *      method (POST|GET|PUT|DELETE) - The request method to accept
-     *      response - a message to display if the request method fails
-     * - example: 
-     *      @request [method=POST, response="This is an error message"]
-     * 
-     * @param string $action
-     * @return Core\Controller
-     */
-    public function getAction($action = "index")
-    {
-        // Set the action to get the annotation
-        $this->setActionName($action);
-
-        $request = $this->getActionAnnotation("request");
-
-        if(is_array($request) && $request["method"]) {
-            $response = $request["response"] ?: "";
-            if (!$this->requestMethod($request["method"], $response)) {
-                return null;
-            } else {
-                return parent::getAction($action);
-            }
-        } else {
-            return parent::getAction($action);
-        }
     }
 }
