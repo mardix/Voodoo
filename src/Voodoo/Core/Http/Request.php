f1baaf7780ee84cc0ef512c066dbe7204d77023b
Mardix <mcx2082@gmail.com>
2012-12-17 23:42:25 -0500

    Added getIp

--- src/Voodoo/Core/Http/Request.php
+++ src/Voodoo/Core/Http/Request.php
@@ -101,4 +101,22 @@ class Request
     {
         return explode("/",$_SERVER["QUERY_STRING"]);
     }
+    
+    /**
+     * Return the request ip
+     * 
+     * @return string
+     */
+    public static function getIp()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+          $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (! empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+          $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }   
+    
 }
