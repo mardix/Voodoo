4db46e1d13598052501381592cdf63536cad43b7
Mardix <mcx2082@gmail.com>
2013-03-10 13:53:39 -0400

    0.15.0

--- src/Voodoo/Core/Voodoo.php
+++ src/Voodoo/Core/Voodoo.php
@@ -28,7 +28,7 @@ class Voodoo
 {
     CONST NAME = "VoodooPHP";
     
-    CONST VERSION = "0.14.4";
+    CONST VERSION = "0.15.0";
     
     CONST AUTHOR = "Mardix < https://github.com/mardix >";
     
