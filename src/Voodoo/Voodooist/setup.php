9df3f45774de1c10be9fc38b032d716e2a8f7e94
Mardix <mcx2082@gmail.com>
2013-06-09 02:58:51 -0400

    Fix setup to install outside of vendor

--- src/Voodoo/Voodooist/setup.php
+++ src/Voodoo/Voodooist/setup.php
@@ -14,18 +14,18 @@
  * @name        Voodooist/setup
  * @desc        A setup file that create your Voodoo environment, include your
  *              App directory, your classes etc.
- *              /PATH/App/Conf/app.json will be created. It a JSON file
+ *              /PATH/App/Conf/app.json will be created. It's a JSON file
  *              that contains your MVC stucture. setup.php will read it and create the files
  *              and directory if not exist.
- *              Also, a /PATH/App/voodooist.php will be created for you to setup your app
+ *              Also, a /App/voodooist.php will be created for you to setup your app
  *
  * @run         To execute this file, run the code below in your command line
- *              cd /PATH/Voodoo/Voodooist
+ *              cd /$PATH/Voodoo/Voodooist
  *              php -f ./setup.php
  *
  *              Once your application is created, next time you want to set it up:
  * 
- *              cd /PATH/App
+ *              cd /$PATH/App
  *              php -f ./voodooist.php
  */
 
@@ -35,9 +35,25 @@ require_once dirname(__DIR__)."/autoload.php";
 
 date_default_timezone_set("America/New_York");
 
-// The application root dir where your application's file will be created at
-$appRootDir = dirname(dirname(__DIR__));
+/**
+ * By default we'll install the /App directory at the your application's root
+ */
 
+// Set to false if Voodoo wasn't installed via composer or is placed somewhere else
+if(! defined("LOAD_VOODOO_WITH_COMPOSER")) {
+    define("LOAD_VOODOO_WITH_COMPOSER", true);
+}
+
+// Assuming the Voodooo is in /vendor/voodoophp/voodoo when installed with Composer, we'll get back in the root
+if(LOAD_VOODOO_WITH_COMPOSER) {
+    $levelUp = 7;
+    $appRootDir = __DIR__;
+    while (max(0, --$levelUp)) {
+        $appRootDir = dirname($appRootDir);
+    }
+} else { // the root dir
+    $appRootDir = dirname(dirname(__DIR__));
+}
 
 try {
     Voodooist\Voodooist::create($appRootDir);
