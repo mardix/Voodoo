08f02f9a9966be6fdb30e3ad80f8bb0786d1c3a3
Mardix <mcx2082@gmail.com>
2014-01-07 17:49:32 -0500

    1.16 - Deprecate _app.* in favor of _.* in the Views

--- src/Voodoo/Core/View.php
+++ src/Voodoo/Core/View.php
@@ -13,18 +13,18 @@
  * -----------------------------------------------------------------------------
  *
  * @name        Core\View
- * @desc        Core View. 
- * 
+ * @desc        Core View.
+ *
  * Mustache Extension
  * == New Markups
  *      === LAYOUT ===
  *      - Layout: To change the layout to another one from Views/_layout directory
  *              {{!use_layout default}} will include _layout/default.html
- * 
+ *
  *      === INCLUDE ===
  *      {{!include $path}} is added to include file using $this->addTemplate during parsing
  *      It's best to use {{!include}} when the included file will contains other customs included
- *      
+ *
  *      $path:
  *          {{!include @action_view}}. Include a file by @name, when it was included
  *                                  directly with $this->addTemplate($name, $src)
@@ -34,7 +34,7 @@
  *                                  /Views of the current module
  *          {{!include /AppName/ModuleName/Views/filename}}. Include a file from
  *                                  a different Appname, with a full path
- *   
+ *
  */
 
 namespace Voodoo\Core;
@@ -42,13 +42,13 @@ namespace Voodoo\Core;
 use Handlebars\Handlebars;
 use Handlebars\Loader\FilesystemLoader;
 
-class View 
+class View
 {
     use View\TView;
-    
+
     const TITLE_CONCAT_PREPEND = 1;
     const TITLE_CONCAT_APPEND = 2;
-    
+
     public $isDisabled = false;
     public $isRendered = false;
 
@@ -62,27 +62,27 @@ class View
     protected $config;
     protected $renderedContent;
     protected $controllersViewPath;
-    
+
     protected $templates =  [];
     protected $templateDir = "";
     protected $parsed = false;
-    protected $definedRaws = []; 
+    protected $definedRaws = [];
 
     private $controller = null;
     private $renderJSON = false;
- 
+
     private $meta = [];
-    
+
     private $templateKeys = [
         "view"      => "action_view",
         "layout"    => "page_layout"
     ];
-    
+
     private $path = [
         "components" => "_components",
         "layouts"  => "_layouts"
     ];
-    
+
     private $engine = null;
     private $flashMessage = null;
 //------------------------------------------------------------------------------
@@ -105,7 +105,7 @@ class View
         $hbOptions = ["extension" => $this->ext];
         $partialsLoader = new FilesystemLoader($this->templateDir, $hbOptions);
         $this->engine = new Handlebars(["partials_loader" => $partialsLoader]);
-                    
+
         /**
          * FlashMessage
          */
@@ -114,7 +114,7 @@ class View
 
     /**
      * To set the title
-     * 
+     *
      * @param string $title
      * @param bool $concat
      * @param int $position
@@ -127,14 +127,14 @@ class View
                 $title = $this->meta["title"] . " " . $title;
             } else if ($position == self::TITLE_CONCAT_PREPEND) {
                 $title = $title . " " . $this->meta["title"];
-            } 
-        } 
+            }
+        }
         return $this->setMeta("title", $title);
     }
-    
+
     /**
      * Set the description
-     * 
+     *
      * @param string $description
      * @return \Voodoo\Core\View
      */
@@ -146,7 +146,7 @@ class View
 
     /**
      * Set the keywords
-     * 
+     *
      * @param array $keywords
      * @return \Voodoo\Core\View
      */
@@ -154,19 +154,19 @@ class View
     {
         return $this->setMeta("keywords", implode(",", array_unique($keywords)));
     }
-    
+
     /**
      * Set the pagination model
-     * 
+     *
      * @param array $data
      * @return View
      */
-    public function setPagination(Array $data) 
+    public function setPagination(Array $data)
     {
-        $this->assign("_app.pagination", $data);
+        $this->assign_("pagination", $data);
         return $this;
     }
-        
+
     /**
      * Set multiple meta data
      * @param type $key
@@ -181,7 +181,7 @@ class View
 
     /**
      * Set a flash message
-     * 
+     *
      * @param type $message
      * @param type $type
      * @param array $data
@@ -192,21 +192,21 @@ class View
         $this->flashMessage->set($message, $type, $data);
         return $this;
     }
-    
+
     /**
      * Get flash message
-     * 
+     *
      * @param string $type - Return flash messages of a type
-     * @return Array
+     * @return Array [message, type, data]
      */
     public function getFlashMessage($type = null)
     {
         return $this->flashMessage->get($type);
     }
-    
+
     /**
      * Clear flash message
-     * 
+     *
      * @return \Voodoo\Core\View
      */
     public function clearFlashMessage()
@@ -214,10 +214,10 @@ class View
         $this->flashMessage->clear();
         return $this;
     }
-    
+
     /**
      * Return the view engine
-     * 
+     *
      * @return Handlebars\Handlebars
      */
     public function getEngine()
@@ -228,7 +228,7 @@ class View
 
     /**
      * Check if the views directory exists
-     * 
+     *
      * @return bool
      */
     public function exists()
@@ -238,7 +238,7 @@ class View
 
     /**
      * Set the view layout to use. By default it will user the contain set in config.
-     * 
+     *
      * @param  string $filename - The name of the layout under _layouts/
      * @return Voodoo\Core\View
      */
@@ -254,18 +254,18 @@ class View
 
     /**
      * Return if the layout is set
-     * 
+     *
      * @return bool
      */
     public function issetLayout()
     {
         return $this->layout ? true : false;
     }
-    
-    
+
+
     /**
      * Set the view body  to use. By default it will use the the action view => $action.html
-     * 
+     *
      * @param  string $filename
      * @param  bool $absolute - true, it will use the full path of filename, or it will look in the current Views
      * @return Voodoo\Core\View
@@ -279,32 +279,32 @@ class View
 
     /**
      * Return if the view is set
-     * 
+     *
      * @return bool
      */
     public function issetActionView()
     {
         return $this->actionView ? true : false;
     }
-    
-    
+
+
     /**
      * Retun bool if the action view file exists
-     * 
+     *
      * @return bool
      */
     public function actionViewExists()
     {
-        $file = ($this->isActionViewAbsolute == true) 
-                ? $this->actionView 
+        $file = ($this->isActionViewAbsolute == true)
+                ? $this->actionView
                 : $this->templateDir.$this->actionView;
         return file_exists($file);
     }
-    
+
     /**
      * To set the error page
-     *  
-     * @param string $errorMessage 
+     *
+     * @param string $errorMessage
      * @param int $errorCode
      * @return Voodoo\Core\View
      */
@@ -316,10 +316,10 @@ class View
         $this->setActionView($this->path["components"]."/error_".$errorCode);
         return $this;
     }
-    
+
     /**
      * To render as JSON
-     * 
+     *
      * @param type $bool
      * @return \Voodoo\Core\View
      */
@@ -328,10 +328,10 @@ class View
         $this->renderJSON = $bool;
         return $this;
     }
-    
+
     /**
      * Check if the view is to be returned as JSON
-     * 
+     *
      * @return bool
      */
     public function isRenderToJson()
@@ -341,39 +341,49 @@ class View
 
     /**
      * Render the template
-     * 
+     *
      * @return String
      */
     public function render()
     {
         if($this->renderJSON) {// RENDER AS JSON
-            $assigned = $this->getAssigned();
-            unset($assigned["_app"]);
-            return json_encode($assigned);
+
+            $this->unassign("_");
+
+            //@deprecated _app.* in favor of _.*
+            $this->unassign("_app");
+
+            return json_encode($this->getAssigned());
+
         } else {// RENDER AS HTML
             // Content already rendered
             if ($this->renderedContent && $this->isRendered) {
                 return $this->renderedContent;
             }
 
-            // _app.title
+            // _.title
             if (isset($this->meta["title"])) {
-                $this->assign("_app.title", $this->meta["title"]);
+                $this->assign_("title", $this->meta["title"]);
             }
 
-             // _app.flashMessage
+             // _.flash_message
             $flashMessage = $this->getFlashMessage();
             if ($flashMessage) {
-                $this->assign("_app.flashMessage", $flashMessage);
+                $this->assign_("flash_message", $flashMessage);
+
+                //@deprecated for naming convetion. Use flash_message
+                $this->assign_("flashMessage", $flashMessage);
+
                 $this->clearFlashMessage();
             }
-            
-            // _app.error
+
+            // _.error
             if ($this->hasError()) {
-                $this->assign("_app.error", $this->getMessage("error"));
+                $this->assign_("error", $this->getMessage("error"));
             }
 
-            $this->assign("_app", [
+            // _.*
+            $this->assign_([
                     "year"              => date("Y"),
                     "site_url"          => $this->controller->getSiteUrl(),
                     "base_url"          => $this->controller->getBaseUrl(),
@@ -390,15 +400,15 @@ class View
 
             $renderName = $this->templateKeys["view"];
             $this->addTemplate(
-                        $this->templateKeys["view"], 
-                        $this->actionView, 
+                        $this->templateKeys["view"],
+                        $this->actionView,
                         $this->isActionViewAbsolute
                     );
 
             if ($this->layout) {
                $renderName = $this->templateKeys["layout"];
                $this->addTemplate(
-                        $this->templateKeys["layout"], 
+                        $this->templateKeys["layout"],
                         $this->layout,
                         $this->isLayoutAbsolute
                     );
@@ -410,24 +420,24 @@ class View
 
             if (isset($this->templates[$renderName])) {
                 $this->renderedContent = $this->engine->render(
-                                                $this->templates[$renderName], 
+                                                $this->templates[$renderName],
                                                 $this->getAssigned()
                                         );
                 // Strip HTML Comments
                 if ($this->controller->getConfig("views.stripHtmlComments")) {
-                   $this->renderedContent = 
+                   $this->renderedContent =
                            Helpers::stripHtmlComments($this->renderedContent);
                 }
             }
-            return $this->renderedContent;            
+            return $this->renderedContent;
         }
     }
 
-   
+
     /**
      * Return the complete file path of the template in the current view.
      * It can be used to access another views from another module
-     * 
+     *
      * @param  strin   $path
      * @return string
      */
@@ -435,10 +445,10 @@ class View
     {
         return $this->controllersViewPath . "/" . strtolower($path) . $this->ext;
     }
-    
+
     /**
      * To add a template file
-     * 
+     *
      * @param  type $name - the name of the template. Can be used to call it: {{!include @name}}
      * @param  type  $src - the filepath relative to the working dir
      * @return Voodoo\Core\View
@@ -447,10 +457,10 @@ class View
     {
         if (! $absolutePath) {
             $controllerName = $this->controller->getControllerName();
-            $src = (preg_match("/^({$controllerName}\/|_[\w]+)/", $src)) 
+            $src = (preg_match("/^({$controllerName}\/|_[\w]+)/", $src))
                                 ? $src : "{$controllerName}/{$src}";
             $src = $this->addExt($this->templateDir . "/" . $src);
-        } 
+        }
         $content = $this->loadFile($src, true);
 
         /**
@@ -463,12 +473,12 @@ class View
             if(preg_match("/{{!use_layout\s+(.*?)\s*}}/i", $content, $matches)){
                $content = str_replace($matches[0], "", $content);
                $this->useLayout($matches[1]);
-           }           
+           }
         }
         $this->addTemplateString($name, $content);
         return $this;
     }
-  
+
     /**
      * Add file extension if ommitted
      * @param  string $file
@@ -504,9 +514,9 @@ class View
             unset($this->templates[$name]);
         }
         return $this;
-    }    
-    
-    
+    }
+
+
     /**
      * To reset the parsing
      * @return Voodoo\Core\View
@@ -535,7 +545,7 @@ class View
 
     /**
      * Parse the template and catch any {{!include }} expression
-     * 
+     *
      * @param string $template
      * @return string
      */
@@ -548,10 +558,10 @@ class View
                         $src = $this->appRootDir . $this->addExt($src);
                     } else if (preg_match("/^\.\.\//", $src)) { // {{!include ../pathFromViewsRoot}}
                         $src = str_replace("..", "", $src);
-                        $src = $this->templateDir . $this->addExt($src);  
+                        $src = $this->templateDir . $this->addExt($src);
                     } else { // {{!include pathOfCurrentControllersViewPath}}
                         $src = $this->controllersViewPath. "/" . $this->addExt($src);
-                    } 
+                    }
                     $tkey = md5($src);
                     if(!isset($this->templates[$tkey])) {
                         $this->addTemplate($tkey, $src, true);
@@ -571,7 +581,7 @@ class View
 
     /**
      * Parse the template
-     * 
+     *
      * @return $this
      */
     private function parse()
@@ -594,11 +604,11 @@ class View
                         }
                     }
                 }
-            }            
+            }
         }
         return $this;
-    }    
- 
+    }
+
     /**
      * To create the module's assets dir
      * Base on the config file
@@ -644,14 +654,39 @@ class View
         // Shared assets starts from the root
         return
             $this->controller->getSiteUrl() . "/" . preg_replace("/^\//", "",
-                    str_replace(Env::getFrontControllerPath(), 
-                                "", 
+                    str_replace(Env::getFrontControllerPath(),
+                                "",
                                 $path ? : Env::getPublicAssetsPath()));
     }
-    
+
     public function __string()
     {
         return $this->render();
     }
-  
+
+    /**
+     * Assign data to _.*
+     * For local use only
+     *
+     * Also deprecate _app.* in favor of _.*
+     *
+     * @param mixed $key
+     * @param mixed $value
+     * @return \Voodoo\Core\View
+     */
+    private function assign_($key, $value = null)
+    {
+        if (is_array($key)) {
+            $data = $key;
+        } else {
+            $data = [$key => $value];
+        }
+        $this->assign("_", $data);
+
+        //@deprecated _app.* in favor of _.*
+        $this->assign("_app", $data);
+
+        return $this;
+    }
+
 }
